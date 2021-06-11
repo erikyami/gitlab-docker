eeb5094c54e3c8b70cd665155158721f31306d4a
Erik Silva <erikyami@gmail.com>
2021-06-11 09:48:10 -0300

    Ajustes variáveis

--- gitlab-docker.sh
+++ gitlab-docker.sh
@@ -7,16 +7,18 @@ mkdir -p /srv/dados
 export GITLAB_HOME=/srv/dados
 export SSH_HOST_PORT=2222
 export GITLAB_HOSTNAME=gitlab.hl.local
+export CONTAINER_NAME=gitlab
+export GITLAB_VERSION=latest
 
 # Comando docker
 docker run --detach \
         --hostname ${GITLAB_HOSTNAME} \
-        --publish 443:443 \
-        --publish 80:80 \
-        --publish ${SSH_HOST_PORT}:22 \
-        --name gitlab \
-        --restart always \
+        --name ${CONTAINER_NAME} \
         --volume ${GITLAB_HOME}/gitlab/config:/etc/gitlab:Z \
         --volume ${GITLAB_HOME}/gitlab/logs:/var/log/gitlab:Z \
         --volume ${GITLAB_HOME}/gitlab/data:/var/opt/gitlab:Z \
-        gitlab/gitlab-ce:latest
+        --publish ${SSH_HOST_PORT}:22 \
+        --publish 443:443 \
+        --publish 80:80 \
+        --restart always \
+        gitlab/gitlab-ce:${GITLAB_VERSION}
