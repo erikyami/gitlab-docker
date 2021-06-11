bc4f6a2b7738b9084f7075671d809435e646d67f
Erik Silva <erikyami@gmail.com>
2021-06-11 07:36:53 -0300

    Comentários no gitlab-runner.sh

--- gitlab-runner/gitlab-runner.sh
+++ gitlab-runner/gitlab-runner.sh
@@ -1,10 +1,16 @@
 #/bin/bash
 
+# Setar o nome do Runner
 export NAME_RUNNER=gitlab-runner-docker
+
+# Setar o nome diretório de configuração
 export CONFIG_DIR=/srv/${NAME_RUNNER}/config
 
+# Criar o diretório de configuração caso não exista
 mkdir -p ${CONFIG_DIR}
 
+
+# Iniciar o gitlab-runner em container
 docker run -d \
   --name ${NAME_RUNNER} \
   --restart always  \
@@ -12,7 +18,5 @@ docker run -d \
   -v /var/run/docker.sock:/var/run/docker.sock \
   gitlab/gitlab-runner:latest
 
-
-
-#register gitlab-runner
+# Após iniciar, é necessário fazer o registro do gitlab-runner
 docker run --rm -it -v ${CONFIG_DIR}:/etc/gitlab-runner gitlab/gitlab-runner register
